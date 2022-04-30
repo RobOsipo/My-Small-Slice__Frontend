14d614919ebcf9568ba2e20f62045d0f8a77a903
Robert <osipovitchr@gmail.com>
2022-04-30 15:17:45 -0400

    created useHover custom hook

--- src/pages/SearchForImages/components/MapImages.js
+++ src/pages/SearchForImages/components/MapImages.js
@@ -1,17 +1,18 @@
 import React, { useContext } from 'react'
 import ImageContext from '../../../context/image-context/image-context'
+import useHover from '../../../hooks/useHover'
 
 import classes from './MapImages.module.css'
 
 const MapImages = () => {
-
+// const [hovered, ref] = useHover()
 const fetchImageContext = useContext(ImageContext)
-console.log('fetchcontext', fetchImageContext)
+
 
 const data = fetchImageContext.map((image, index) => (
     <>
     {/* <div className={classes.images} style={{ backgroundImage: "url('image.url')"}}></div> */}
-    <img className={classes.images} src={image.url} alt={image.title} />
+    <img className={classes.images} src={image.url} alt={image.title}  />
     </>
 ))
 
