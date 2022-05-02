ad8c6638141e95f3eb6cf89d36217ee3df549e02
Robert <osipovitchr@gmail.com>
2022-05-02 10:34:45 -0400

    fixed search images input bug + added form

--- src/pages/SearchForImages/SearchForImages.js
+++ src/pages/SearchForImages/SearchForImages.js
@@ -5,19 +5,39 @@ import MapImages from './components/MapImages'
 import classes from './SearchForImages.module.css'
 
 const SearchForImages = () => {
-    const [searchTerm, setSearchTerm] = useState('final fantasy')
+    const [searchTerm, setSearchTerm] = useState('')
+    const [submitSearch, setSubmitSearch] = useState('final fantasy')
+    const [loadingFlag, setLoadingFlag] = useState(false)
 
     const handleChange = (e) => {
         setSearchTerm(e.target.value)
     }
+
+    const handleSubmit = (e) =>  {
+        e.preventDefault()
+        setLoadingFlag(true)
+        setSubmitSearch(searchTerm)
+
+        setTimeout(() => {
+            setLoadingFlag(false)
+        }, 3000)
+        
+    }
+
+
+
     // todo: left searchBy prop hardcoded to prevent too many request
   return (
-      <ImageContextProvider searchBy="Final Fantasy"> 
+      <ImageContextProvider searchBy={submitSearch}> 
 
         <section className={classes['main-container']}>
             <Header />
-            <label className={classes.label} htmlFor="search">Search for Images Here</label>
-            <input className={classes.input} name="search" value={searchTerm} onChange={(e) => handleChange(e)} />
+            <form onSubmit={handleSubmit}>
+                <label className={classes.label} htmlFor="search">Search for Images Here</label>
+                <input className={classes.input} name="search" value={searchTerm} onChange={(e) => handleChange(e)} />
+                <button type="submit" value="submit">Click to Search</button>
+            </form>
+            {loadingFlag && <h2 className={classes.loading}>Please wait while we fetch your photos</h2>}
             <section className={classes['images-container']}>
                 <MapImages />
             </section>
