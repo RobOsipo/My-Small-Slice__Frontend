ebaf2e817e66ef4f4f55024b6190997ba2ed54de
Robert <osipovitchr@gmail.com>
2022-05-11 16:16:17 -0400

    rough draft  mini-games page styling

--- src/pages/PlayAGame/PlayAGame.js
+++ src/pages/PlayAGame/PlayAGame.js
@@ -5,8 +5,10 @@ import RpsContextProvider from '../../context/rps-context/RpsContextProvider'
 import RpsGame from './components/RpsGame/RpsGame'
 import Tenzies from './components/Tenzies/Tenzies'
 import Game from './components/TicTacToe/Game' // tic-tac-toe import
-import retroImage from '../../localImages/retro2.jpg'
-
+// non-components
+import pacman from '../../localImages/retro2.jpg'
+import donkeykong from '../../localImages/retro3.jpg'
+import largepacman from '../../localImages/retro4.jpg'
 import classes from './PlayAGame.module.css'
 const PlayAGame = () => {
   const [speedType, setSpeedType] = useState(false)
@@ -63,23 +65,25 @@ const PlayAGame = () => {
       <span className={classes['retro-buttons']}>B</span>
     </div>
 
-    <img src={retroImage} alt="anything" className={classes['retro-image']} />
+    <img src={pacman} alt="anything" className={classes.pacman} />
+    <img src={donkeykong} alt="anything" className={classes['donkey-kong']} />
+    <img src={largepacman} alt="anything" className={classes['large-pacman']} />
 
-        <h1 className={classes.title}>A List of Mini-Games Coded from Scratch For Your Entertainment</h1>
+        <h1 className={classes.title}>Play Mini-Games</h1>
 
         <button onClick={toggleSpeedTypeGame} className={classes['start-btn']}>{!speedType ? 'Play' : 'Close'} The Speed Typing Game</button>
         
         {speedType && <WordGame />}
 
-        <button onClick={toggleRps} className={classes['start-btn']}>{!speedType ? 'Play' : 'Close'} Rock Paper Scissors</button>
+        <button onClick={toggleRps} className={classes['start-btn']}>{!playRps ? 'Play' : 'Close'} Rock Paper Scissors</button>
         
         {playRps && <RpsContextProvider setRpsChoice={setRpsChoice} choice={rpsChoice}> <RpsGame setRpsChoice={setRpsChoice} /> </RpsContextProvider>}
 
-        <button onClick={toggleTenzies} className={classes['start-btn']}>{!speedType ? 'Play' : 'Close'} Tenzies</button>
+        <button onClick={toggleTenzies} className={classes['start-btn']}>{!playTenzies ? 'Play' : 'Close'} Tenzies</button>
        
         {playTenzies && <Tenzies />}
 
-        <button onClick={toggleTTT} className={classes['start-btn']}>{!speedType ? 'Play' : 'Close'} 2 Player Tic-Tac-Toe</button>
+        <button onClick={toggleTTT} className={classes['start-btn']}>{!playTTT ? 'Play' : 'Close'} 2 Player Tic-Tac-Toe</button>
         
           { playTTT && <Game /> }
       </section>
