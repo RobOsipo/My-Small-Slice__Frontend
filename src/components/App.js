e5929a2d80da99c53ed1af46f56ce6f41ca58dc0
Robert <osipovitchr@gmail.com>
2022-05-11 12:40:50 -0400

    routes after login switched to ProtectedRoutes

--- src/components/App.js
+++ src/components/App.js
@@ -1,66 +1,61 @@
-import React from 'react'
-import { Switch, Route, Redirect } from 'react-router-dom'
-import Welcome from '../pages/Welcome/Welcome'
-import SearchForImages from '../pages/SearchForImages/SearchForImages'
-import Notes from '../pages/Notes/Notes'
-import PlayAGame from '../pages/PlayAGame/PlayAGame'
-import Login from '../pages/Login/Login'
-import Choose from '../pages/Choose/Choose'
-import NoteContextProvider from '../context/note-context/NoteContextProvider'
+import React from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import Welcome from "../pages/Welcome/Welcome";
+import SearchForImages from "../pages/SearchForImages/SearchForImages";
+import Notes from "../pages/Notes/Notes";
+import PlayAGame from "../pages/PlayAGame/PlayAGame";
+import Login from "../pages/Login/Login";
+import Choose from "../pages/Choose/Choose";
+import NoteContextProvider from "../context/note-context/NoteContextProvider";
 
-import cookie from 'cookie'
-import './App.css'
+import cookie from "cookie";
+import "./App.css";
 
 export const checkAuth = () => {
-  const cookies = cookie.parse(document.cookie)
-  return cookies["loginToken"] ? true : false
-}
+  const cookies = cookie.parse(document.cookie);
+  return cookies["loginToken"] ? true : false;
+};
 
-const ProtectedRoute = ({component: Component, ...rest}) => {
+const ProtectedRoute = ({ component: Component, ...rest }) => {
   return (
-    <Route 
+    <Route
       {...rest}
-      render={(props) => checkAuth()
-      ? <Component {...props} />
-      : <Redirect to="/login" />
+      render={(props) =>
+        checkAuth() ? <Component {...props} /> : <Redirect to="/login" />
       }
     />
-  )
-}
-
+  );
+};
 
 function App() {
   const [note, setNote] = React.useState({
     title: "",
-    content: ""
+    content: "",
   });
 
   return (
-    <>
-    
-        <Switch>
-          <Route exact path="/">
-              <Welcome /> 
-          </Route>
-          <Route path="/login">
-              <Login />
-          </Route>
+    <NoteContextProvider note={note}>
+      <Switch>
+        <Route exact path="/">
+          <Welcome />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
 
-          <ProtectedRoute path="/choose" component={Choose} />
-  
-          <Route path="/search-for-images">
-              <SearchForImages />
-           </Route>
-          <Route path="/notes"> 
-            <NoteContextProvider note={note}>
-                <Notes note={note} setNote={setNote} />
-            </NoteContextProvider>
-          </Route>
-          <Route path="/play-a-game"> 
-              <PlayAGame />
-           </Route>
-        </Switch>
-    </>
+        <ProtectedRoute path="/choose" component={Choose} />
+
+        <ProtectedRoute path="/search-for-images" component={SearchForImages} />
+
+        <ProtectedRoute path="/play-a-game" component={PlayAGame} />
+
+        <Route path="/notes">
+          <NoteContextProvider note={note}>
+            <Notes note={note} setNote={setNote} />
+          </NoteContextProvider>
+        </Route>
+      </Switch>
+    </NoteContextProvider>
   );
 }
 
